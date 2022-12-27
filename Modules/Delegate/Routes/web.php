cfb6c5cfe66e3c26e1c7cf8f798f824c37bc315e
Eliot8 <aldersone39@gmail.com>
2022-12-27 02:51:13 +0100

    week payment request update

--- Modules/Delegate/Routes/web.php
+++ Modules/Delegate/Routes/web.php
@@ -45,8 +45,15 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function 
         $week_orders = \Modules\Delegate\Entities\WeekOrders::where('delivery_man_id', $delegate_id)
             ->where('week_end', $week_end)
             ->first();
+        $delivery_man =  \Modules\Delegate\Entities\Delegate::find($week_orders->delivery_man_id);
+
+        $discount_system_earnings = $week_orders->system_earnings - ($delivery_man->commission_earnings + $week_orders->personal_earnings);
+        
+        $delivery_man->commission_earnings = 0;
+        $delivery_man->save();
+
         $week_orders->personal_earnings = 0;
-        $week_orders->system_earnings = 0;
+        $week_orders->system_earnings = $discount_system_earnings < 0 ? 0 : $discount_system_earnings;
         $week_orders->save();
 
         flash(Lang::get('delegate::delivery.payment_request_success'), 'success');
